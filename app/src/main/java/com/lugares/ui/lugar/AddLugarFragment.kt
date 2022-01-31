3a1fe2bdfa2b16168d24b818b2af0e24ac55dc17
wmoli <uso.docente.2022@gmail.com>
2022-01-31 00:47:11 -0600

    Semana 02, fundamentos de SQLite.. add lugar

---
+++ app/src/main/java/com/lugares/ui/lugar/AddLugarFragment.kt
@@ -0,0 +1,50 @@
+package com.lugares.ui.lugar
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import com.lugares.R
+import com.lugares.databinding.FragmentAddLugarBinding
+import com.lugares.databinding.FragmentLugarBinding
+import com.lugares.model.Lugar
+import com.lugares.viewmodel.LugarViewModel
+
+class AddLugarFragment : Fragment() {
+
+    private var _binding: FragmentAddLugarBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var lugarViewModel: LugarViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAddLugarBinding.inflate(inflater, container, false)
+
+        lugarViewModel = ViewModelProvider(this).get(LugarViewModel::class.java)
+
+        binding.btAgregar.setOnClickListener { addLugar() }
+
+        return binding.root
+
+    }
+
+    private fun addLugar() {
+        var nombre = binding.etNombre.text.toString()
+        if (nombre.isNotEmpty()) {  //Podemos insertar el lugar
+            var correo = binding.etCorreo.text.toString()
+            var telefono = binding.etTelefono.text.toString()
+            var web = binding.etWeb.text.toString()
+            val lugar = Lugar(0,nombre,correo,telefono,web,0.0,0.0,0.0,"","")
+            lugarViewModel.addLugar(lugar)
+            Toast.makeText(requireContext(),getString(R.string.msg_lugar_agregado),Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(requireContext(),getString(R.string.msg_faltan_datos),Toast.LENGTH_LONG).show()
+        }
+    }
+}
---
+++ app/src/main/java/com/lugares/ui/lugar/LugarFragment.kt
@@ -0,0 +1,45 @@
+package com.lugares.ui.lugar
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.lugares.R
+import com.lugares.databinding.FragmentLugarBinding
+import com.lugares.viewmodel.LugarViewModel
+
+class LugarFragment : Fragment() {
+
+    private lateinit var lugarViewModel: LugarViewModel
+    private var _binding: FragmentLugarBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        lugarViewModel =
+            ViewModelProvider(this)[LugarViewModel::class.java]
+
+        _binding = FragmentLugarBinding.inflate(inflater, container, false)
+
+        binding.fbAgregar.setOnClickListener {
+            findNavController().navigate(R.id.action_nav_lugar_to_addLugarFragment)
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
