c96b8aac4cd4166189ed9044f92417c49440320b
wmoli <uso.docente.2022@gmail.com>
2022-02-23 21:57:11 -0600

    Semana 045 REcursos

--- app/src/main/java/com/lugares/ui/lugar/AddLugarFragment.kt
+++ app/src/main/java/com/lugares/ui/lugar/AddLugarFragment.kt
@@ -1,13 +1,19 @@
 package com.lugares.ui.lugar
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.lugares.R
 import com.lugares.databinding.FragmentAddLugarBinding
 import com.lugares.model.Lugar
@@ -31,19 +37,56 @@ class AddLugarFragment : Fragment() {
             agregarLugar()
         }
 
+        ubicaGPS()
+
         return binding.root
     }
 
+    private var conPermisos:Boolean=true;
+    private fun ubicaGPS() {
+        val fusedLocationProviderClient : FusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireContext())
+        if (ActivityCompat.checkSelfPermission(requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION),105)
+        }
+        if (conPermisos) {
+            fusedLocationProviderClient.lastLocation.addOnSuccessListener {
+                location: Location? ->
+                if (location != null) {
+                    binding.tvLatitud.text="${location.latitude}"
+                    binding.tvLongitud.text="${location.longitude}"
+                    binding.tvAltura.text="${location.altitude}"
+                } else {
+                    binding.tvLatitud.text=getString(R.string.error)
+                    binding.tvLongitud.text=getString(R.string.error)
+                    binding.tvAltura.text=getString(R.string.error)
+                }
+            }
+        }
+    }
+
+
     private fun agregarLugar() {
-        val nombre=binding.etNombre.text.toString()
+        val nombre = binding.etNombre.text.toString()
         if (nombre.isNotEmpty()) {
-            val correo=binding.etCorreo.text.toString()
-            val telefono=binding.etTelefono.text.toString()
-            val web=binding.etWeb.text.toString()
-            val lugar= Lugar(0,nombre,correo,telefono,web,0.0,
-                0.0,0.0,"","")
+            val correo = binding.etCorreo.text.toString()
+            val telefono = binding.etTelefono.text.toString()
+            val web = binding.etWeb.text.toString()
+            val latitud = binding.tvLatitud.text.toString().toDouble()
+            val longitud = binding.tvLongitud.text.toString().toDouble()
+            val altura = binding.tvAltura.text.toString().toDouble()
+
+            val lugar = Lugar(0, nombre, correo, telefono, web,
+                latitud,longitud,altura,
+                "", "")
             lugarViewModel.addLugar(lugar)
-            Toast.makeText(requireContext(),
+            Toast.makeText(
+                requireContext(),
                 getString(R.string.msg_lugar_add),
                 Toast.LENGTH_SHORT
             ).show()
--- app/src/main/java/com/lugares/ui/lugar/UpdateLugarFragment.kt
+++ app/src/main/java/com/lugares/ui/lugar/UpdateLugarFragment.kt
@@ -1,6 +1,10 @@
 package com.lugares.ui.lugar
 
+import android.Manifest
 import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
@@ -35,37 +39,116 @@ class UpdateLugarFragment : Fragment() {
         binding.etTelefono.setText(args.lugar.telefono)
         binding.etWeb.setText(args.lugar.web)
 
-        binding.btUpdateLugar.setOnClickListener {
-            actualizarLugar()
-        }
+        binding.tvLongitud.text=args.lugar.longitud.toString()
+        binding.tvLatitud.text=args.lugar.latitud.toString()
+        binding.tvAltura.text=args.lugar.altura.toString()
+
+        binding.btUpdateLugar.setOnClickListener { actualizarLugar() }
+        binding.btEmail.setOnClickListener { escribirCorreo() }
+        binding.btPhone.setOnClickListener { llamarLugar() }
+        binding.btWhatsapp.setOnClickListener { enviarWhatsApp() }
+        binding.btWeb.setOnClickListener { verWeb() }
+        binding.btLocation.setOnClickListener { verMapa() }
 
         setHasOptionsMenu(true)  //Este fragmento debe tener un menu adicional
-
         return binding.root
     }
 
+    private fun escribirCorreo() {
+        val para = binding.etCorreo.text.toString()
+        if (para.isNotEmpty()) {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "message/rfc822"
+            intent.putExtra(Intent.EXTRA_EMAIL,arrayOf(para))
+            intent.putExtra(Intent.EXTRA_SUBJECT,
+            getString(R.string.msg_saludos)+" "+binding.etNombre.text)
+            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.msg_mensaje_correo))
+            startActivity(intent)
+        } else {
+            Toast.makeText(requireContext(),getString(R.string.msg_datos),Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun llamarLugar() {
+        val telefono = binding.etTelefono.text.toString()
+        if (telefono.isNotEmpty()) {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:$telefono")
+            if (requireActivity().checkSelfPermission(Manifest.permission.CALL_PHONE) !=
+                    PackageManager.PERMISSION_GRANTED) {
+                        requireActivity()
+                            .requestPermissions(arrayOf(Manifest.permission.CALL_PHONE),105)
+            } else {
+                requireActivity().startActivity(intent)
+            }
+        } else {
+            Toast.makeText(requireContext(),getString(R.string.msg_datos),Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun enviarWhatsApp() {
+        val telefono = binding.etTelefono.text.toString()
+        if (telefono.isNotEmpty()) {
+            val intent = Intent(Intent.ACTION_VIEW)
+            val uri ="whatsapp://send?phone=506$telefono&text="+
+                getString(R.string.msg_saludos)
+            intent.setPackage("com.whatsapp")
+            intent.data=Uri.parse(uri)
+            startActivity(intent)
+        } else {
+            Toast.makeText(requireContext(),getString(R.string.msg_datos),Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+    private fun verWeb() {
+        val sitio = binding.etWeb.text.toString()
+        if (sitio.isNotEmpty()) {
+            val uri =Uri.parse("http://$sitio")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        } else {
+            Toast.makeText(requireContext(),getString(R.string.msg_datos),Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun verMapa() {
+        val latitud = binding.tvLatitud.text.toString().toDouble()
+        val longitud = binding.tvLongitud.text.toString().toDouble()
+        if (latitud.isFinite() && longitud.isFinite()) {
+            val location =Uri.parse("geo:$latitud,$longitud?z=18")
+            val intent = Intent(Intent.ACTION_VIEW, location)
+            startActivity(intent)
+        } else {
+            Toast.makeText(requireContext(),getString(R.string.msg_datos),Toast.LENGTH_LONG).show()
+        }
+    }
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.delete_menu,menu)
+        inflater.inflate(R.menu.delete_menu, menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         //Se es delete...
-        if (item.itemId==R.id.delete_menu) {
+        if (item.itemId == R.id.delete_menu) {
             deleteLugar()
         }
         return super.onOptionsItemSelected(item)
     }
 
     private fun actualizarLugar() {
-        val nombre=binding.etNombre.text.toString()
+        val nombre = binding.etNombre.text.toString()
         if (nombre.isNotEmpty()) {
-            val correo=binding.etCorreo.text.toString()
-            val telefono=binding.etTelefono.text.toString()
-            val web=binding.etWeb.text.toString()
-            val lugar= Lugar(args.lugar.id,nombre,correo,telefono,web,0.0,
-                0.0,0.0,"","")
+            val correo = binding.etCorreo.text.toString()
+            val telefono = binding.etTelefono.text.toString()
+            val web = binding.etWeb.text.toString()
+            val lugar = Lugar(
+                args.lugar.id, nombre, correo, telefono, web, 0.0,
+                0.0, 0.0, "", ""
+            )
             lugarViewModel.updateLugar(lugar)
-            Toast.makeText(requireContext(),
+            Toast.makeText(
+                requireContext(),
                 getString(R.string.msg_lugar_update),
                 Toast.LENGTH_SHORT
             ).show()
@@ -76,9 +159,9 @@ class UpdateLugarFragment : Fragment() {
     private fun deleteLugar() {
         val builder = AlertDialog.Builder(requireContext())
         builder.setTitle(R.string.menu_delete)
-        builder.setMessage(getString(R.string.msg_seguroBorrar)+ " ${args.lugar.nombre}?")
-        builder.setNegativeButton(getString(R.string.no)) {_,_ ->}
-        builder.setPositiveButton(getString(R.string.si)) {_,_ ->
+        builder.setMessage(getString(R.string.msg_seguroBorrar) + " ${args.lugar.nombre}?")
+        builder.setNegativeButton(getString(R.string.no)) { _, _ -> }
+        builder.setPositiveButton(getString(R.string.si)) { _, _ ->
             lugarViewModel.deleteLugar(args.lugar)
             findNavController().navigate(R.id.action_nav_updateLugar_to_nav_lugar)
         }
